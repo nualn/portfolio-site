ff36b13559835d97105c961981f7fa9e2ef28cc6
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-10-27 01:24:11 +0300

    Add TLS support

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 This is (going to be) my portfolio site. It's currently very much a skeleton to have something to build on.
 
-It's deployed on Google Compute Engine.
+It's deployed on a Google Compute Engine VM and accessible at [nikkola.dev](https://nikkola.dev).
+
+What I might do next:
+
+- [ ] Build a deployment system with Kubernetes to make it easier to host multiple projects on the same VM.
+- [ ] add more content
+    - [ ] projects
+    - [ ] CV
+
--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,18 @@
 
 image_name="server_image"
 container_name="server_container"
+zip_name="server.zip"
 vm_user=$(gcloud compute os-login describe-profile | grep username | awk '{ print $2 }')
 server_ip=$(gcloud compute instances list | grep personal-site | awk '{ print $5 }')
 
-docker build -t $image_name .
+zip -r server.zip . -x "deploy.sh" -x "README.md" -x ".git"
 
-docker save -o $image_name.tar $image_name
-
-scp $image_name.tar $vm_user@$server_ip:$image_name.tar
+scp $zip_name $vm_user@$server_ip:$zip_name
 
 ssh $vm_user@$server_ip /bin/sh << EOF
-    sudo docker load < $image_name.tar
-    sudo docker stop $container_name
-    sudo docker rm $container_name
-    sudo docker run -d --name $container_name -p 80:80 $image_name
-    sudo docker image prune -f
+    sudo unzip -o $zip_name -d ./server
+    cd server
+    sudo docker-compose restart
 EOF
 
-rm $image_name.tar
+rm server.zip
